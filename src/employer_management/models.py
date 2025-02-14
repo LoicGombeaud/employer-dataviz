f79efae368c0e754c460eb67e9806219a701eaef
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-14 18:24:11 +0100

    feat(employer_management): Add basic models

--- src/employer_management/models.py
+++ src/employer_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Employer(models.Model):
+    name = models.CharField(max_length=100)
+
+class Address(models.Model):
+    street_address = models.CharField(max_length=100)
+
+class EmployerSite(models.Model):
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+class Employee(models.Model):
+    employer_site = models.ForeignKey(EmployerSite, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
