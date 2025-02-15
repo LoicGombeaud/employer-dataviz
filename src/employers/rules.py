76c06cd21ba6d47616f15be688c1e9842f0e2873
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-15 08:26:53 +0100

    feat(rules): Refine rules

--- src/employers/rules.py
+++ src/employers/rules.py
@@ -15,9 +15,9 @@ def is_employer_liaison(user, employer):
     except:
         return False
 
-is_superuser_or_territory_liaison_or_employer_liaison = rules.is_superuser | is_territory_liaison | is_employer_liaison
+is_territory_liaison_or_employer_liaison = is_territory_liaison | is_employer_liaison
 
-rules.add_perm('employers.create_employer', is_superuser_or_territory_liaison_or_employer_liaison)
-rules.add_perm('employers.view_employer', is_superuser_or_territory_liaison_or_employer_liaison)
-rules.add_perm('employers.change_employer', is_superuser_or_territory_liaison_or_employer_liaison)
-rules.add_perm('employers.delete_employer', is_superuser_or_territory_liaison_or_employer_liaison)
+rules.add_perm("employers.add_employer", is_territory_liaison)
+rules.add_perm("employers.view_employer", is_territory_liaison_or_employer_liaison)
+rules.add_perm("employers.change_employer", is_territory_liaison_or_employer_liaison)
+rules.add_perm("employers.delete_employer", is_territory_liaison)
