4b704a64ae253d95cc49409252b81e1ba11ca612
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-15 01:56:34 +0100

    feat(models): Add string representation

--- src/employer_management/models.py
+++ src/employer_management/models.py
@@ -3,13 +3,22 @@ from django.db import models
 class Employer(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Address(models.Model):
     street_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.street_address
 
 class EmployerSite(models.Model):
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.employer.name + " - " + self.name
 
 class Employee(models.Model):
     employer_site = models.ForeignKey(EmployerSite, on_delete=models.CASCADE)
