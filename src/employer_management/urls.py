b064268779b7b093964166c421bb7b92773645f5
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-15 02:30:04 +0100

    feat: Add employer view

--- src/employer_management/urls.py
+++ src/employer_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:employer_id>", views.employer, name="employer")
 ]
--- src/employer_management/views.py
+++ src/employer_management/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Employer
+
 def index(request):
     return HttpResponse("This is the employer management index.")
+
+def employer(request, employer_id):
+    employer = Employer.objects.get(id=employer_id)
+    return HttpResponse(f"Employer #{employer_id}: {employer.name}")
