5b67e56b67626483996ea6d48528ea682bc4c95e
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-15 07:03:04 +0100

    feat(models): Add string representation for liaisons

--- src/employers/models.py
+++ src/employers/models.py
@@ -22,9 +22,15 @@ class EmployerLiaison(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.employer) + " - " + str(self.user)
+
 class TerritoryLiaison(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     territory = models.ForeignKey(Territory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.territory) + " - " + str(self.user)
 
 class Address(models.Model):
     street_address = models.CharField(max_length=100,
