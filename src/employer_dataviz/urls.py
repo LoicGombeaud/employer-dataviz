a0ec7741daf341bd7ae05b99573a3bc1e4ffd2b9
Loïc Gombeaud <loic.gombeaud@gmail.com>
2025-02-15 09:00:43 +0100

    feat: Add (non-admin) login page

--- src/employer_dataviz/urls.py
+++ src/employer_dataviz/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
 
 urlpatterns = [
     path("employers/", include("employers.urls")),
     path("admin/", admin.site.urls),
+    path(
+        'accounts/login/',
+        LoginView.as_view(
+            template_name='admin/login.html',
+            extra_context={
+                'title': 'Login',
+                'site_title': 'Employer Dataviz',
+                'site_header': 'Employer Dataviz',
+            }),
+            name='login'),
 ]
